f3fa8b0e128115b5bee60e605fb7504ffc9218ec
Max Nosov <mnosov88@gmail.com>
2020-10-02 02:35:59 +0300

    first solution commit

---
+++ Lesson4_task2/Program.cs
@@ -0,0 +1,80 @@
+﻿/*
+ Задание 2
+Используя Visual Studio, создайте проект по шаблону Console Application.
+Напишите программу - консольный калькулятор.
+Создайте две переменные с именами operand1 и operand2. Задайте переменным некоторые
+произвольные значения. Предложите пользователю ввести знак арифметической операции. Примите
+значение, введенное пользователем, и поместите его в строковую переменную sign.
+Для организации выбора алгоритма вычислительного процесса, используйте переключатель switch.
+Выведите на экран результат выполнения арифметической операции.
+В случае использования операции деления, организуйте проверку попытки деления на ноль. И если
+таковая имеется, то отмените выполнение арифметической операции и уведомите об ошибке
+пользователя.
+ */
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Lesson4_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter sign: ");
+            string sign = Convert.ToString(Console.ReadLine());
+            string[] array = { "+", "-", "*", "/" };
+
+            if (array.Contains(sign))
+            {
+                Console.WriteLine("Please, enter value of operand1: ");
+                double operand1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please, enter value of operand1: ");
+                double operand2 = Convert.ToDouble(Console.ReadLine());
+
+                double result = 0;
+
+                switch (sign)
+                {
+                    case "+":
+                        {
+                            result = operand1 + operand2;
+                            break;
+                        }
+                    case "-":
+                        {
+                            result = operand1 - operand2;
+                            break;
+                        }
+                    case "*":
+                        {
+                            result = operand1 * operand2;
+                            break;
+                        }
+                    case "/":
+                        {
+                            if (operand2 == 0)
+                            {
+                                Console.WriteLine("Division by 0 is not acceptable");
+                            }
+                            else
+                            {
+                                result = operand1 / operand2;
+                            }
+                            break;
+                        }
+                }
+                Console.WriteLine("Great! Your result is {0}.", result);
+                Console.ReadKey();
+
+            } 
+            else 
+            {
+                Console.WriteLine("You entered not valid sign. Try one of those: +, -, *, /");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Lesson4_task3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lesson4_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value from 0 to 100: ");
+            double value;
+
+            try
+            {
+                value = Convert.ToDouble(Console.ReadLine());
+
+                if (value >= 0 && value <= 14)
+                {
+                    Console.WriteLine("Entered by you number is contained in 0-14 range");
+                    Console.ReadKey();
+                }
+                else if (value >= 15 && value <= 35)
+                {
+                    Console.WriteLine("Entered by you number is contained in 15-35 range");
+                    Console.ReadKey();
+                }
+                else if (value >= 36 && value <= 50)
+                {
+                    Console.WriteLine("Entered by you number is contained in 36-50 range");
+                    Console.ReadKey();
+                }
+                else if (value >= 50 && value <= 100)
+                {
+                    Console.WriteLine("Entered by you number is contained in 50-100 range");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("You entered out of range value. Try again :) ");
+                    Console.ReadKey();
+                }
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine("You entered not valid value. Exception occurred: {0}", e.Message);
+                Console.WriteLine("Please, try to enter digit value");
+                Console.ReadKey();
+            }
+        }
+    }
+}
